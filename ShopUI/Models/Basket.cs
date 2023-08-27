0cc9af4384eea23cac79f2ed817e59c46779d3cd
Amin <KahramAmin@gmail.com>
2023-08-27 09:40:52 +0330

    Basket and Basket Item created

---
+++ ShopUI/Models/Basket.cs
@@ -0,0 +1,45 @@
+﻿namespace ShopUI.Models
+{
+    public class Basket
+    {
+        private List<BasketItem> _items = new();
+        public void Add (Product product,ushort quantity)
+        {
+            var item = _items.Where(prod=>prod.Id == product.Id).FirstOrDefault();
+            if (item != null)
+            {
+                item.Quantity += quantity;
+            }
+            else
+            {
+                _items.Add
+                    (
+                        new BasketItem
+                        {
+                            Id = product.Id,
+                            Quantity = quantity,
+                            Product = product,
+                        }
+                    );
+            }
+        }
+        
+        public void Remove(int Productid)
+            => _items.RemoveAll(prod => prod.Id == Productid);
+
+        public int TotalPrice()
+            => _items.Sum(pr => pr.Quantity * pr.Product.Price);
+
+        public void Clear()
+            => _items.Clear();
+        
+        public IEnumerable<BasketItem> GetBasketItems() => _items;
+    }
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public ushort Quantity { get; set; }
+    }
+     
+}
