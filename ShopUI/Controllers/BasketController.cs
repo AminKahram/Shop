772c730db8ceda8395a1ba647669164aa7a5d34d
Amin <KahramAmin@gmail.com>
2023-08-29 10:17:06 +0330

    Global using added and sessionExtention implemented

--- ShopUI/Controllers/BasketController.cs
+++ ShopUI/Controllers/BasketController.cs
@@ -1,58 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using ShopUI.Models;
+﻿namespace ShopUI.Controllers; 
 
-namespace ShopUI.Controllers
+public class BasketController : Controller
 {
-    public class BasketController : Controller
+    private readonly IProductRepository productRepository;
+     
+    public BasketController(IProductRepository productRepository)
     {
-        private readonly IProductRepository productRepository;
-        
-        public BasketController(IProductRepository productRepository)
-        {
-            this.productRepository = productRepository;
-        }
-        public IActionResult Index(string returnUrl)
-        {
-            var basket = GetBasket();
-            BasketViewModel viewModel = new BasketViewModel
-            { 
-                Basket = basket,
-                ReturnUrl = returnUrl
-            };
-            return View(viewModel);
-        }
-        public IActionResult AddToBasket(int productId, string returnUrl)
-        {
-            var product = productRepository.GetProductById(productId);
-            var basket = GetBasket();
-            basket.Add(product, 1);
-            SaveBasket(basket);
-            return RedirectToAction("Index", new { returnUrl = returnUrl });
-        }
+        this.productRepository = productRepository;
+    }
+    public IActionResult Index(string returnUrl)
+    {
+        var basket = GetBasket();
+        BasketViewModel viewModel = new BasketViewModel
+        { 
+            Basket = basket,
+            ReturnUrl = returnUrl
+        };
+        return View(viewModel);
+    }
+    public IActionResult AddToBasket(int productId, string returnUrl)
+    {
+        var product = productRepository.GetProductById(productId);
+        var basket = GetBasket();
+        basket.Add(product, 1);
+        SaveBasket(basket);
+        return RedirectToAction("Index", new { returnUrl = returnUrl });
+    }
 
-        
+    
 
-        public IActionResult RemoveFromBasket(int productId)
-        {
-            var product = productRepository.GetProductById(productId).Id;
-            var basket = GetBasket();
-            basket.Remove(product);
-            SaveBasket(basket);
-            return RedirectToAction("Index");
-        }
-        private Basket GetBasket()
-        {
-            var basketString = HttpContext.Session.GetString("Basket");
-            if (string.IsNullOrEmpty(basketString))
-            {
-                return new Basket();
-            }
-            return JsonConvert.DeserializeObject<Basket>(basketString);
-        }
-        private void SaveBasket(Basket basket)
-        {
-            HttpContext.Session.SetString("Basket", JsonConvert.SerializeObject(basket));
-        }
+    public IActionResult RemoveFromBasket(int productId)
+    {
+        var product = productRepository.GetProductById(productId).Id;
+        var basket = GetBasket();
+        basket.Remove(product);
+        SaveBasket(basket); 
+        return RedirectToAction("Index");
+    }
+    private Basket GetBasket()
+    {
+       return HttpContext.Session.GetJson<Basket>("Basket");
+    }
+    private void SaveBasket(Basket basket)
+    {
+        HttpContext.Session.SetJson("Basket", basket);
     }
 }
---
+++ ShopUI/GlobalUsing.cs
@@ -0,0 +1,3 @@
+﻿global using ShopUI.Models;
+global using Microsoft.AspNetCore.Mvc;
+global using ShopUI.Extentions;
