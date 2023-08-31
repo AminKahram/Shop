c7d057f52459cac72ef431b3233920ef71e46ebe
Amin <KahramAmin@gmail.com>
2023-08-31 06:33:04 +0330

    Order deatail added to orders and made its controller with checkout action

--- ShopUI/Migrations/20230831025845_AddOrderWithDetail.Designer.cs
+++ ShopUI/Migrations/20230831025845_AddOrderWithDetail.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopUI.Models;
 
@@ -11,9 +12,10 @@ using ShopUI.Models;
 namespace ShopUI.Migrations
 {
     [DbContext(typeof(StoreDBContext))]
-    partial class StoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230831025845_AddOrderWithDetail")]
+    partial class AddOrderWithDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
